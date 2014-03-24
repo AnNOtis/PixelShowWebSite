daa6dad5772860942017a034caf73336249fe2f7
AnNOtis <otis@hdtech.com.tw>
2014-03-24 21:05:17 +0800

    add gem - 'bcrypt-ruby', '3.0.1'

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
 
+gem 'bcrypt-ruby', '3.0.1'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save { seld.email = email.downcase }
 	validates :name, presence:true,length:{ in: 3..20 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence:true,format:{ with:VALID_EMAIL_REGEX }, uniqueness: { case_sensitive:false }
