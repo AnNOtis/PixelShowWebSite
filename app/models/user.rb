e9bca79dfb9807899c2c95ad0d3dfccf659342d0
AnNOtis <otis@hdtech.com.tw>
2014-03-24 21:21:27 +0800

    user has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
 	validates :name, presence:true,length:{ in: 3..20 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence:true,format:{ with:VALID_EMAIL_REGEX }, uniqueness: { case_sensitive:false }
+
+	has_secure_password
+	validates :password, length:{ minimum:6 }
 end
