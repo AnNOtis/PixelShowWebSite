11b8978512441f5e4a9da3e7aa979f5d366515d3
AnNOtis <otis@hdtech.com.tw>
2014-03-24 20:17:25 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PixelShowWeb::Application.config.secret_key_base = '0b4006cf5dcc22f30619aac6b4f59ca82d51ec742c1cbbbdb5ab424fadfa6991372e3d1ca008ae5c2c3c5b2ff65e0c1142c524a9898b8e7889b859ddb07ceb11'
