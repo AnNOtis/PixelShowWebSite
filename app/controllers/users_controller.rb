8acbeccc438a62f5c2df75840c91993f6847afe3
AnNOtis <otis@hdtech.com.tw>
2014-03-25 09:44:43 +0800

    controller User add CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
   def new
+  	@user = User.new
+  end
+  def create
+  	@user = User.new(params[:event])
+  	@user.save
+
+  	redirect_to 'home'
+  end
+  def update
+  	@user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 PixelShowWeb::Application.routes.draw do
-  get "users/new"
-  get "static_pages/home"
+  resources :users
   get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
