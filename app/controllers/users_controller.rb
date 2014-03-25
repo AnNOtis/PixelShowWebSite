e50c5ec30bece1573fd60c5f4c4f95d3fa4ba193
AnNOtis <otis@hdtech.com.tw>
2014-03-25 10:44:45 +0800

    add signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,17 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   def create
-  	@user = User.new(params[:event])
+  	@user = User.new(user_params)
   	@user.save
 
   	redirect_to 'home'
   end
   def update
   	@user = User.find(params[:id])
+  end
+
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	before_save { seld.email = email.downcase }
+	before_save { self.email = email.downcase }
 	validates :name, presence:true,length:{ in: 3..20 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence:true,format:{ with:VALID_EMAIL_REGEX }, uniqueness: { case_sensitive:false }
