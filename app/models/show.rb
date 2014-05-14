67850a5f07b64f8bbb273400cac080e7f4519ba5
Otis <otis@hdtech.com.tw>
2014-05-15 00:16:14 +0800

    shows model order by created time

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 	belongs_to :user
 	validates :name, presence: true
-
+	default_scope { order("created_at DESC") } #按照創建時間排序
 	before_save :init_data
 	private
 	def init_data
