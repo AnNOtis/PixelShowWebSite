9347b3473f02d17bd44568cbd9cec35c456837dc
Otis <otis@hdtech.com.tw>
2014-05-15 21:39:51 +0800

    fix fonts icon

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module PixelShowWeb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     #config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-    Bundler.require(:default, :assets, Rails.env) 
+    config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
   end
 end
