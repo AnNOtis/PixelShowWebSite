af8bb8a7e8c56659761c293261a6f8dbdd3d7cb7
Otis <otis@hdtech.com.tw>
2014-05-15 17:57:19 +0800

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module PixelShowWeb
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    #config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    Bundler.require(:default, :assets, Rails.env) 
   end
 end
