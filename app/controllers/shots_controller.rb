3096d0de0e5af8216b21d4d0c1bd5f340a9470a0
Otis <otis@hdtech.com.tw>
2014-05-18 16:15:30 +0800

    modify ui

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,8 +1,13 @@
 class ShotsController < ApplicationController
 
 	def show
-		@shot = Show.friendly.find(params[:id])
-		@shot.person_number+=1
-		@shot.save
+		begin
+			@shot = Show.find(params[:id])
+			@shot.person_number+=1
+			@shot.save
+		rescue ActiveRecord::RecordNotFound  
+			redirect_to root_path
+			return
+		end
 	end
 end
