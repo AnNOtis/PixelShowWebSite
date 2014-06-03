1f2ab9b8885114a25fa3f9e3947a49a953298e3d
Otis <otis@hdtech.com.tw>
2014-06-03 23:07:14 +0800

    add timeframe radio button

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,13 +1,16 @@
 class ShotsController < ApplicationController
 	def index
+		binding.pry
+		timeframe = params[:timeframe]
 		@shots = Show.all.sample(8)
 	end
 	def show
-		
-			@shot = Show.find(params[:id])
+		@shot = Show.find(params[:id])
+		if signed_in?
 			@is_like = @shot.likes.find_by(user_id:current_user.id).nil? ? false : true
-			@comments = @shot.comments.order(created_at: :desc).all
-			@shot.person_number+=1
-			@shot.save
+		end
+		@comments = @shot.comments.order(created_at: :desc).all
+		@shot.person_number+=1
+		@shot.save
 	end
 end
