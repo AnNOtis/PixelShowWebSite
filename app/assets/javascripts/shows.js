7c95a3296b71eaa6f96d9b2435b4d44cf14a1601
Taian Su <me@taian.su>
2014-06-03 16:25:53 +0800

    fix tab and spaces

--- app/assets/javascripts/shows.js
+++ app/assets/javascripts/shows.js
@@ -1,148 +1,148 @@
 //顯示show
 var ShowWidget={};
 ShowWidget = {
-	settings:{
+    settings:{
 
-	},
-	init: function(){
+    },
+    init: function(){
 
-	},
-	generateShow: function(s_canvas, size, showData){
-		//$(s_canvas).empty();
-		
-		var myCanvas = d3.select(s_canvas);
-		var canvasLength = $(s_canvas).attr("width");	
-		myCanvas.selectAll("g").remove();
-		myCanvas.selectAll("line").remove();
+    },
+    generateShow: function(s_canvas, size, showData){
+        //$(s_canvas).empty();
 
-		var rowG = myCanvas.selectAll("g")
-		.data(showData,function(row,yPosition){
-			$.each(row,function(xPosition,value){
-				var cell = {};
-				cell.x = xPosition;
-				cell.y = yPosition;
-				cell.value = value;
-				row[xPosition] = cell;
-			});
-			return showData;
-		})
-		.enter()
-		.append('g')
-		.attr('height',function(d) {
-			return canvasLength/size;
-		})
-		.attr('x',0)
-		.attr('y',function(d,i) {
-			return i*canvasLength/size;
-		});
+        var myCanvas = d3.select(s_canvas);
+        var canvasLength = $(s_canvas).attr("width");
+        myCanvas.selectAll("g").remove();
+        myCanvas.selectAll("line").remove();
 
-		//fill color
-		rowG.selectAll('rect')
-		.data(function(d) {
-			// console.log(d);
-			return d;
-		})
-		.enter().append('rect')
-		.attr({
-	    width: canvasLength/size,
-	    height: canvasLength/size
-	  })
-		.attr({
-		  x: function (d) { return d.x*canvasLength/size; },
-		  y: function (d) { return d.y*canvasLength/size; },
-		})
-		.attr({
-		  "data-x": function (d) { return d.x; },
-		  "data-y": function (d) { return d.y; },
-		  fill: function (d) { return d.value; }
-		});
-	},
-	generateAxis: function(s_canvas){
-		// console.log("generateAxis");
-		var canvas = d3.select(s_canvas);
-		canvas.selectAll('.xAxis').data(d3.range(0,size+1))
-		.enter()
-		.append('line')
-		.attr('class','axis')
-		.attr('x1',function(d) {
-			return 0;
-		})
-		.attr('y1',function(d) {
-			return d*length/size;
-		})
-		.attr('x2',function(d) {
-			return length;
-		})
-		.attr('y2',function(d) {
-			return d*length/size;
-		});
+        var rowG = myCanvas.selectAll("g")
+        .data(showData,function(row,yPosition){
+            $.each(row,function(xPosition,value){
+                var cell = {};
+                cell.x = xPosition;
+                cell.y = yPosition;
+                cell.value = value;
+                row[xPosition] = cell;
+            });
+            return showData;
+        })
+        .enter()
+        .append('g')
+        .attr('height',function(d) {
+            return canvasLength/size;
+        })
+        .attr('x',0)
+        .attr('y',function(d,i) {
+            return i*canvasLength/size;
+        });
+
+        //fill color
+        rowG.selectAll('rect')
+        .data(function(d) {
+            // console.log(d);
+            return d;
+        })
+        .enter().append('rect')
+        .attr({
+        width: canvasLength/size,
+        height: canvasLength/size
+      })
+        .attr({
+          x: function (d) { return d.x*canvasLength/size; },
+          y: function (d) { return d.y*canvasLength/size; },
+        })
+        .attr({
+          "data-x": function (d) { return d.x; },
+          "data-y": function (d) { return d.y; },
+          fill: function (d) { return d.value; }
+        });
+    },
+    generateAxis: function(s_canvas){
+        // console.log("generateAxis");
+        var canvas = d3.select(s_canvas);
+        canvas.selectAll('.xAxis').data(d3.range(0,size+1))
+        .enter()
+        .append('line')
+        .attr('class','axis')
+        .attr('x1',function(d) {
+            return 0;
+        })
+        .attr('y1',function(d) {
+            return d*length/size;
+        })
+        .attr('x2',function(d) {
+            return length;
+        })
+        .attr('y2',function(d) {
+            return d*length/size;
+        });
 
 
-		canvas.selectAll('.yAxis').data(d3.range(0,size+1))
-		.enter()
-		.append('line')
-		.attr('class','axis')
-		.attr('x1',function(d) {
-			return d*length/size;
-		})
-		.attr('y1',function(d) {
-			return 0;
-		})
-		.attr('x2',function(d) {
-			return d*length/size;
-		})
-		.attr('y2',function(d) {
-			return length;
-		});
-	},
-	convertStrToCodeArr: function(dataString) {
-		// console.log(dataString);
-		var tempArr = dataString.slice(1,dataString.length).split("_");
-		var resultArr = [];
-		$.each(tempArr,function(index,value){
-			var noPrefixArr = value.slice(1,value.length).split("#");
-			$.each(noPrefixArr,function(index,value){
-				noPrefixArr[index] = "#"+value;
-			})
-			//console.log(noPrefixArr);
-			resultArr.push(noPrefixArr);
-		});
-		return resultArr;
-	},
-	//@param canvas:svg element
-	convertImgToStr: function(canvas){
-		result = "";
-		$('#canvas').find('g').each(function(){
-			result+="_";
-			$(this).find('rect').each(function(){
-				var color = $(this).attr('fill');
-				result+= color;
-			})
-		})
-		return result;
-	},
-	convertImgToArr: function(canvas){
- 		var result = [];
- 		$(canvas).find('g').each(function(){
- 			var rowArr = []
- 			$(this).find('rect').each(function(){
- 				var color = $(this).attr('fill');
-  			rowArr.push(color);
- 			})
- 			result.push(rowArr);
- 		})
- 		return result;
- 	},
-	autoFillBlock: function(canvas,x,y,fillColor){
-		x = parseInt(x);
-		y = parseInt(y);
-		dataArr = ShowWidget.convertImgToArr(canvas);
-		var preColor = dataArr[y][x];
-		ShowWidget.calFillBlock(x,y,preColor,fillColor);
-		ShowWidget.generateShow(canvas,10,dataArr);
-		ShowWidget.generateAxis(canvas);
-		console.log('!!!');
-	},
+        canvas.selectAll('.yAxis').data(d3.range(0,size+1))
+        .enter()
+        .append('line')
+        .attr('class','axis')
+        .attr('x1',function(d) {
+            return d*length/size;
+        })
+        .attr('y1',function(d) {
+            return 0;
+        })
+        .attr('x2',function(d) {
+            return d*length/size;
+        })
+        .attr('y2',function(d) {
+            return length;
+        });
+    },
+    convertStrToCodeArr: function(dataString) {
+        // console.log(dataString);
+        var tempArr = dataString.slice(1,dataString.length).split("_");
+        var resultArr = [];
+        $.each(tempArr,function(index,value){
+            var noPrefixArr = value.slice(1,value.length).split("#");
+            $.each(noPrefixArr,function(index,value){
+                noPrefixArr[index] = "#"+value;
+            })
+            //console.log(noPrefixArr);
+            resultArr.push(noPrefixArr);
+        });
+        return resultArr;
+    },
+    //@param canvas:svg element
+    convertImgToStr: function(canvas){
+        result = "";
+        $('#canvas').find('g').each(function(){
+            result+="_";
+            $(this).find('rect').each(function(){
+                var color = $(this).attr('fill');
+                result+= color;
+            })
+        })
+        return result;
+    },
+    convertImgToArr: function(canvas){
+         var result = [];
+         $(canvas).find('g').each(function(){
+             var rowArr = []
+             $(this).find('rect').each(function(){
+                 var color = $(this).attr('fill');
+              rowArr.push(color);
+             })
+             result.push(rowArr);
+         })
+         return result;
+     },
+    autoFillBlock: function(canvas,x,y,fillColor){
+        x = parseInt(x);
+        y = parseInt(y);
+        dataArr = ShowWidget.convertImgToArr(canvas);
+        var preColor = dataArr[y][x];
+        ShowWidget.calFillBlock(x,y,preColor,fillColor);
+        ShowWidget.generateShow(canvas,10,dataArr);
+        ShowWidget.generateAxis(canvas);
+        console.log('!!!');
+    },
     outsideOfCanvas: function(x, y, size){
         console.log('meet edge');
         return x < 0 || y < 0 || x >= size || y >= size;
