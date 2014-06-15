432ae5335cf5421d3b83ba6f908bbbe3aac0a2fa
Otis <otis@hdtech.com.tw>
2014-06-16 00:31:24 +0800

    edit user profile operation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_login
   def show
   	@user = User.find(params[:id])
   end
@@ -18,11 +19,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def update
-  	@user = User.update(user_params)
+    if( User.update(params[:id], user_update_params) )
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   private
     def user_params
     	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def user_update_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
