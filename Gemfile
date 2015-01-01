275151e21b727c3fb3f02fd9a8be15a09015651a
Otis <otis@hdtech.com.tw>
2015-01-01 19:57:03 +0800

    add figaro

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem "figaro"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  auth = YAML.load(File.open("#{Rails.root}/config/omniauth.yml"))[Rails.env]
   provider :developer unless Rails.env.production?
-  provider :facebook, auth['facebook']['app_id'],auth['facebook']['app_secret'] 
+  provider :facebook, ENV["facebook_app_id"],ENV["facebook_app_secret"]
 end
