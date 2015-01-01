6064678680ca1b5ccf36bad7ca9c016091d7f57c
Otis <otis@hdtech.com.tw>
2015-01-01 20:04:35 +0800

    update counters

--- db/migrate/20140613145651_add_likes_count_to_show.rb
+++ db/migrate/20140613145651_add_likes_count_to_show.rb
@@ -1,13 +1,13 @@
 class AddLikesCountToShow < ActiveRecord::Migration
-  def self.up    
+  def self.up
     add_column :shows, :likes_count, :integer, :default => 0
-    
+
     # 如果是網站上線後才新增這個功能，這裡需要先計算設定好初始值
     Show.find_each do |show|
-      show.update_attribute(:likes_count, show.likes.count)
+      Show.update_counters(show.id, likes_count: show.likes.count)
     end
   end
-  
+
   def self.down
     remove_column :shows, :likes_count
   end
--- db/migrate/20140618143916_add_comments_count_to_show.rb
+++ db/migrate/20140618143916_add_comments_count_to_show.rb
@@ -1,12 +1,12 @@
 class AddCommentsCountToShow < ActiveRecord::Migration
-  def self.up    
+  def self.up
     add_column :shows, :comments_count, :integer, :default => 0
-        
+
     Show.find_each do |show|
-      show.update_attribute(:comments_count, show.comments.count)
+      Show.update_counters(show.id, comments_count: show.comments.count)
     end
   end
-  
+
   def self.down
     remove_column :shows, :comments_count
   end
