8a57998b9bff66778c9d11b8a9bb3cd1a775c956
Otis <otis@hdtech.com.tw>
2015-01-03 12:46:18 +0800

    add undo redo

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -23,6 +23,7 @@ EditorWidget = {
   bindUIActions: function(){
     var toggleMouseDown = false;//記錄滑鼠是否被按下
     _this = this;
+
     $(es.canvasID).on('mousedown','rect',function(){
       if(es.currentMode == es.FILL_MODE){
         ShowWidget.autoFillBlock('svg.canvas',$(this).attr('data-x'),$(this).attr('data-y'),es.currentColor);
@@ -31,11 +32,17 @@ EditorWidget = {
         $(this).attr("fill",es.currentColor);
       }
     });
+
     $(es.canvasID).on('mouseover','rect',function(){
       if(toggleMouseDown){
         $(this).attr("fill",es.currentColor);
       }
     });
+
+    $(document).on('mouseup', function(){
+      toggleMouseDown = false;
+    });
+
     $(es.canvasID).on('mouseup','rect',function(){
       toggleMouseDown = false;
       $(this).attr("fill",es.currentColor);
@@ -43,9 +50,9 @@ EditorWidget = {
       EditorWidget.sendDataToServer($(es.canvasID));
     });
 
-    $(".mode-btn").click(function() {
+    $(".js-mode-btn").click(function() {
       es.currentMode = $(this).attr("data-mode");
-      $(".mode-btn").removeClass('active');
+      $(".js-mode-btn").removeClass('active');
       $(this).addClass('active');
     });
 
@@ -53,21 +60,41 @@ EditorWidget = {
       $("#show_data").val(ShowWidget.convertImgToStr($(es.canvasID)));
       $("#edit_show_"+$('#show_id').val()).submit();
     });
+
     $("a#download-png").click(function() {
       EditorWidget.setUrlToDownloadField();
     });
+
     $("#undoBtn").click(function(){
       _this.undo();
     });
+
     $("#redoBtn").click(function(){
       _this.redo();
     });
+  },
+  presentUndoRedoState: function(){
+    $("#undoBtn").removeClass('is_disabled');
+    $("#redoBtn").removeClass('is_disabled');
+
+    if(es.stacks.length <= 1){
+      $("#undoBtn").addClass('is_disabled');
+      $("#redoBtn").addClass('is_disabled');
+
+    }else if(es.stackIndex === 0){
+      $("#undoBtn").addClass('is_disabled');
+
+    }else if(es.stackIndex === es.stacks.length-1){
+      $("#redoBtn").addClass('is_disabled');
+    }
+
   },
   pushImgToStack: function(){
     var data = ShowWidget.convertImgToStr(es.currentCanvas);
     es.stackIndex += 1;
     es.stacks.splice(es.stackIndex, es.stacks.length);
     es.stacks.push(data);
+    this.presentUndoRedoState();
   },
   undo: function(){
     var targetIndex = es.stackIndex - 1;
@@ -77,6 +104,7 @@ EditorWidget = {
     es.stackIndex = targetIndex;
     var targetImg = es.stacks[targetIndex];
     this.overwriteCanvas(targetImg);
+    this.presentUndoRedoState();
   },
   redo: function(){
     var targetIndex = es.stackIndex + 1;
@@ -86,6 +114,7 @@ EditorWidget = {
     es.stackIndex = targetIndex;
     var targetImg = es.stacks[targetIndex];
     this.overwriteCanvas(targetImg);
+    this.presentUndoRedoState();
   },
   sendDataToServer: function(canvas){
     var postFormUrl = $("#edit_show_"+$('#show_id').val()).attr('action');
@@ -108,5 +137,6 @@ EditorWidget = {
   overwriteCanvas: function(data){
     var showData = ShowWidget.convertStrToCodeArr(data);
     ShowWidget.generateShow( es.canvasID , 10, showData);
+    this.sendDataToServer(this.currentCanvas())
   }
 }
